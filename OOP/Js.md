55e03b2500101c7b0034672263dd5356c917e6e6
Palla04 <128132131+Palla04@users.noreply.github.com>
2024-07-26 07:39:35 +0000

    OOP done

---
+++ OOP/Js.md
@@ -0,0 +1,28 @@
+# Does JavaScript really have classes?
+  Yes, JavaScript does have classes. They were introduced in ECMAScript 6 (ES6) to provide a simpler and more intuitive syntax for creating objects and handling inheritance. Although JavaScript has always been a prototype-based language, classes offer a more traditional object-oriented approach, making it easier for developers familiar with other object-oriented languages to work with JavaScript.
+
+## OOP
+
+## Object
+- collection of properties and method
+- toLowerCase
+
+## Why use OOP
+
+## parts of OOP
+Object Literal
+
+- constructor function
+- Prototypes
+- Classes
+- Instances (new,this)
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
+
+
+### this
+In JavaScript, the keyword this is used to refer to the current context, which in the case of an object method, refers to the object itself. When you use this inside a method, it allows you to access the properties and other methods of the object that the method belongs to.
---
+++ OOP/oop.js
@@ -0,0 +1,50 @@
+// Object literal
+
+const user = { 
+   // Object Properties
+    userName : "Pallabi",
+    loginCount: 8,
+    signedIn : true,
+  
+  // Object Method
+    getUserDetails : function(){
+         console.log(`Username: ${this.userName}`);    // 'this' refers to the 'user' object or the current Object
+         console.log(this);
+    }
+}
+console.log(user.userName);
+console.log(user.getUserDetails());
+console.log(this);
+
+
+const user2 = { 
+  // Object Properties
+   userName : "Pallabi",
+   loginCount: 8,
+   signedIn : true,
+ 
+ // Object Method
+   getUserDetails : function(){
+        console.log(`Username: ${this.userName}`);    // 'this' refers to the 'user' object or the current Object
+        console.log(this);
+   }
+}
+
+
+function User(username, loginCount, isLoggedIn){
+  this.username = username
+  this.loginCount = loginCount
+  this.isLoggedIn = isLoggedIn
+
+  this.greeting = function(){
+    console.log(`welcome ${this.username}`);
+  }
+
+  //return this      // If we return or not it implicitly returns
+}
+
+const userOne = new User("Piku", 12, true)  //In JavaScript, 'new' is used to create an instance of an object from a constructor function.
+const userTwo = new User("Soma",11,false)   //Without the new keyword, the this inside the constructor function refers to the global object (in non-strict mode) or
+                                            //is undefined (in strict mode). Consequently, the properties username, loginCount, and isLoggedIn are assigned to the global object, 
+console.log(userOne.greeting());
+userOne.greeting()
