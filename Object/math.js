ac80d4cd3c4ab43c76bd5b80f1b0f030b4b70d9d
Pallabi Biswas <128132131+Palla04@users.noreply.github.com>
2024-08-07 05:20:47 +0000

    property descriptor complete

--- Object/math.js
+++ Object/math.js
@@ -1,6 +1,10 @@
 const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
 console.log(descriptor);
 
+//The Math.PI property in JavaScript is not writable because it is defined 
+//with specific property descriptors that make it non-writable, non-enumerable,
+// and non-configurable. This means you cannot change its value, list it in 
+//a loop over properties, or reconfigure its descriptors.
 const myNewObject = {
     name: 'ginger chai',
     price: 250,
@@ -14,7 +18,7 @@ console.log(myNewObject);
 
 console.log(Object.getOwnPropertyDescriptor(myNewObject,"name"));
 Object.defineProperty(myNewObject, 'name', {
-    //Writable: false,
+    writable: false,
     enumerable: true
 })
 
